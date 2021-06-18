0251c119e5a13647667ba26cf57bf8847ead9b3d
Benjamin15122 <15051183877@163.com>
2021-06-18 23:59:10 +0800

    add schema form and link

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -1,17 +1,26 @@
-import App from './App.js'
-import OutgoingList from './pages/OutgoingList.js'
-import Outgoing from './pages/Outgoing.js'
+import App from "./App.js";
+import OutgoingList from "./pages/OutgoingList.js";
+import Outgoing from "./pages/Outgoing.js";
+import SchemaBoard from "./pages/SchemaBoard.js";
 
-const routes = [{
+const routes = [
+  {
     path: "/",
     exact: true,
-    component: App
-}, {
+    component: App,
+  },
+  {
     path: "/outgoinglist",
     component: OutgoingList,
-}, {
+  },
+  {
     path: "/outgoing",
-    component: Outgoing
-}]
+    component: Outgoing,
+  },
+  {
+    path: "/schemaboard",
+    component: SchemaBoard,
+  },
+];
 
 export default routes;
