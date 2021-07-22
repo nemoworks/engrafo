49511d251344eec7f8e929ae4f39cdeaf195c648
王国畅 <wangguochang@wangguochangdeMacBook-Pro.local>
2021-07-22 17:44:35 +0800

    add swagger-ui

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/graph", graphRouter);
+app.use("/swagger-ui", express.static("./swagger-ui"));
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- backend/routes/graph.js
+++ backend/routes/graph.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const graph = require("../data-structure/Graph");
-const router = express.Router();
+var router = express.Router();
 
 /* GraphList */
 router.get("/list", function (req, res, next) {
