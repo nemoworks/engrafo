31c07a28cb238e89db8703703f1bd4efca7d2767
Benjamin15122 <15051183877@163.com>
2021-08-10 14:35:08 +0800

    fix app booting

--- backend/app.js
+++ backend/app.js
@@ -42,10 +42,4 @@ app.use(function (err, req, res, next) {
   res.render("error");
 });
 
-const port = 8080;
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
-
 module.exports = app;
