c6467e3b4318ca3167c77d7aca0663be775529bb
Benjamin15122 <15051183877@163.com>
2021-08-17 14:28:22 +0800

    add cross origin

--- backend/app.js
+++ backend/app.js
@@ -12,6 +12,14 @@ var app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By",' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
 app.set("view engine", "pug");
 
 app.use(logger("dev"));
