0a6573a290e0302fc786cdcc42003b07b11df5b1
Benjamin15122 <15051183877@163.com>
2021-08-24 18:21:25 +0800

    fix enkrino self link

--- backend/routes/outgoing.js
+++ backend/routes/outgoing.js
@@ -1,128 +1,168 @@
 const express = require("express");
 const { outgoing, db } = require("../database");
-const LCengine = require("enkrino")
+const LCengine = require("../LCengine");
 var router = express.Router();
 
 router.get("/list", async function (req, res) {
-  db.any(outgoing.findAll).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  db.any(outgoing.findAll)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 router.get("/:id", async function (req, res) {
-  db.one(outgoing.find, req.params.id).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  db.one(outgoing.find, req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 router.get("/nexts/:id", async function (req, res) {
-  db.one(outgoing.find, req.params.id).then(
-    data => {
-      let { lifecycle } = data
-      res.send(LCengine.nexts(lifecycle))
-    }
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.one(outgoing.find, req.params.id)
+    .then((data) => {
+      let { lifecycle } = data;
+      res.send(LCengine.nexts(lifecycle.enkrino));
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.post("/start/:id", async function (req, res) {
-  db.one(outgoing.find, req.params.id).then(
-    data => {
-      let { lifecycle, formdata } = data
-      db.one(outgoing.update, [req.params.id, JSON.stringify(formdata), JSON.stringify(LCengine.start(lifecycle))]).then(
-        data => res.send(data)
-      ).catch(
-        err => { console.error(err); res.send({ message: err.toString() }) }
-      )
-    }
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.one(outgoing.find, req.params.id)
+    .then((data) => {
+      let { lifecycle, formdata } = data;
+      db.one(outgoing.update, [
+        req.params.id,
+        JSON.stringify(formdata),
+        JSON.stringify({
+          ...lifecycle,
+          enkrino: LCengine.start(lifecycle.enkrino),
+        }),
+      ])
+        .then((data) => res.send(data))
+        .catch((err) => {
+          console.error(err);
+          res.send({ message: err.toString() });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.post("/next/:id/:nid", async function (req, res) {
-  db.one(outgoing.find, req.params.id).then(
-    data => {
-      let { lifecycle, formdata } = data
-      db.one(outgoing.update, [req.params.id, JSON.stringify(formdata), JSON.stringify(LCengine.next(lifecycle, req.params.nid))]).then(
-        data => res.send(data)
-      ).catch(
-        err => { console.error(err); res.send({ message: err.toString() }) }
-      )
-    }
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.one(outgoing.find, req.params.id)
+    .then((data) => {
+      let { lifecycle, formdata } = data;
+      db.one(outgoing.update, [
+        req.params.id,
+        JSON.stringify(formdata),
+        JSON.stringify({
+          ...lifecycle,
+          enkrino: LCengine.next(lifecycle.enkrino, req.params.nid),
+        }),
+      ])
+        .then((data) => res.send(data))
+        .catch((err) => {
+          console.error(err);
+          res.send({ message: err.toString() });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.post("/finish/:id", async function (req, res) {
-  db.one(outgoing.find, req.params.id).then(
-    data => {
-      let { lifecycle, formdata } = data
-      db.result(outgoing.update, [req.params.id, JSON.stringify(formdata), JSON.stringify(LCengine.finish(lifecycle))]).then(
-        data => res.send(data)
-      ).catch(
-        err => { console.error(err); res.send({ message: err.toString() }) }
-      )
-    }
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.one(outgoing.find, req.params.id)
+    .then((data) => {
+      let { lifecycle, formdata } = data;
+      db.result(outgoing.update, [
+        req.params.id,
+        JSON.stringify(formdata),
+        JSON.stringify({
+          ...lifecycle,
+          enkrino: LCengine.finish(lifecycle.enkrino),
+        }),
+      ])
+        .then((data) => res.send(data))
+        .catch((err) => {
+          console.error(err);
+          res.send({ message: err.toString() });
+        });
+    })
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.post("/", async function (req, res) {
-  const { formdata, lifecycle } = req.body
-  db.one(outgoing.insert, [JSON.stringify(formdata), JSON.stringify(lifecycle)]).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  const { formdata, lifecycle } = req.body;
+  db.one(outgoing.insert, [JSON.stringify(formdata), JSON.stringify(lifecycle)])
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 router.put("/:id", async function (req, res) {
-  const { formdata, lifecycle } = req.body
-  db.one(outgoing.update, [req.params.id, JSON.stringify(formdata), JSON.stringify(lifecycle)]).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  const { formdata, lifecycle } = req.body;
+  db.one(outgoing.update, [
+    req.params.id,
+    JSON.stringify(formdata),
+    JSON.stringify(lifecycle),
+  ])
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 router.get("/formdata/list", async function (req, res) {
-  db.any(outgoing.findAllData).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.any(outgoing.findAllData)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.get("/formdata/:id", async function (req, res) {
-  db.one(outgoing.findData, req.params.id).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
-})
+  db.one(outgoing.findData, req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
+});
 
 router.put("/formdata/:id", async function (req, res) {
-  db.one(outgoing.updateData, [req.params.id, JSON.stringify(req.body)]).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  db.one(outgoing.updateData, [req.params.id, JSON.stringify(req.body)])
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 router.delete("/:id", async function (req, res) {
-  db.result(outgoing.delete, req.params.id).then(
-    data => res.send(data)
-  ).catch(
-    err => { console.error(err); res.send({ message: err.toString() }) }
-  )
+  db.result(outgoing.delete, req.params.id)
+    .then((data) => res.send(data))
+    .catch((err) => {
+      console.error(err);
+      res.send({ message: err.toString() });
+    });
 });
 
 module.exports = router;
