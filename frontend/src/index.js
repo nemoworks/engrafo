43fe49c57a4851cd20a6c7f0dfa5da35cd71c477
Benjamin15122 <15051183877@163.com>
2021-08-30 16:49:46 +0800

    disable react dev tools

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,34 @@ import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme";
 import Layout from "./Layout";
+function disableReactDevTools() {
+  // Check if the React Developer Tools global hook exists
+  if (typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__ !== "object") {
+    return;
+  }
+
+  for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+    if (prop === "renderers") {
+      // this line will remove that one console error
+
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map()
+    } else {
+      // Replace all of its properties with a no-op function or a null value
+      // depending on their types
+
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] =
+        typeof window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] === "function"
+          ? () => {}
+          : null;
+    }
+  }
+}
+
 if (
   process.env.NODE_ENV === "production" &&
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ &&
-  Object.keys(window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers).length
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__
 ) {
-  window.__REACT_DEVTOOLS_GLOBAL_HOOK__._renderers = {};
+  disableReactDevTools()
 }
 
 ReactDOM.render(
