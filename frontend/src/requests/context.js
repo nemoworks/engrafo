970049712714903bded78a6dc33aac7cd270ebb0
Benjamin15122 <15051183877@163.com>
2021-08-30 16:52:23 +0800

    tag v0.0.2

--- frontend/src/requests/context.js
+++ frontend/src/requests/context.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST=process.env.NODE_ENV==="production"?'localhost:21081':'localhost:8080'
+const HOST=process.env.NODE_ENV==="production"?'114.212.80.19:21081':'localhost:8080'
 
 const api=`http://${HOST}/api/context/`
 
