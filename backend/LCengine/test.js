cf416df83ad8698913c855f891c07011edc1e8cc
NoBugzzZ <739651338@qq.com>
2021-10-25 13:40:00 +0800

    datasheet add span

---
+++ backend/LCengine/test.js
@@ -0,0 +1,238 @@
+const e = require("express");
+const _ = require("lodash");
+const successors = (graph, id) => {
+  let result = new Set();
+  graph.edges.forEach((element) => {
+    if (element.from === id) {
+      result.add(element.to);
+    }
+  });
+  return Array.from(result);
+};
+
+const reachableNodes = (graph, id) => {
+  let adj = {};
+  graph.edges.forEach((element) => {
+    if (!adj[element.from]) {
+      adj[element.from] = [];
+    }
+    adj[element.from].push(element.to);
+  });
+  if (!adj[id]) {
+    return [];
+  }
+  let queue = [id];
+  let visited = new Set();
+  visited.add(id);
+  let res = new Set();
+  while (queue.length > 0) {
+    v = queue.pop();
+    if (!adj[v]) {
+      continue;
+    }
+    adj[v].forEach((element) => {
+      if (!visited.has(element)) {
+        visited.add(element);
+        queue.push(element);
+        res.add(element);
+      }
+    });
+  }
+  return Array.from(res);
+};
+
+const fromString = (strlist) => {
+  let graph = {
+    nodes: [],
+    edges: [],
+  };
+  let adj = {};
+  strlist.forEach((str) => {
+    [from, to] = str.split("->");
+    if (!adj[from]) {
+      adj[from] = [];
+      graph.nodes.push({
+        id: from,
+        name: `node-${from}`,
+      });
+    }
+    if (!adj[to]) {
+      adj[to] = [];
+      graph.nodes.push({
+        id: to,
+        name: `node-${to}`,
+      });
+    }
+    graph.edges.push({
+      from: from,
+      to: to,
+    });
+  });
+  return graph;
+};
+const m = {
+  /**
+    @param graph {nodes:[],edges[]}
+    @param id String
+     */
+  start: (enkrino, context) => {
+    let newenkrino = {
+      ...enkrino,
+      status: "running",
+      current: enkrino.start,
+      currentAuth: enkrino.graph.nodes.find(e=>e.id===enkrino.start).auth,
+      graph: {
+        ...enkrino.graph,
+        edges: enkrino.graph.edges.filter((e) => e.spec.temporary === false),
+      },
+      mirror: {
+        nodes: enkrino.graph.nodes.map((e) => e.id!==enkrino.start?{
+            ...e,
+            stack: [],
+          }:{
+            ...e,
+            stack: [context]
+          }),
+        edges: [],
+      },
+    };
+    return newenkrino;
+  },
+
+  next: (enkrino, nid, context) => {
+    let sus = successors(enkrino.graph, enkrino.current);
+    let newenkrino = {};
+    if (sus.includes(nid)) {
+      //forward
+      let eset = enkrino.mirror.edges;
+      eset.push({
+        from: nid,
+        to: enkrino.current,
+      });
+      newenkrino = {
+        ...enkrino,
+        current: nid,
+        currentAuth: enkrino.graph.nodes.find(e=>e.id===nid).auth,
+        mirror: {
+          nodes: enkrino.mirror.nodes.map((e) => e.id!==nid?e:{
+            ...e,
+            stack: e.stack.concat([context])
+          }),
+          edges: _.uniqWith(eset, (A, B) => A.from === B.from && A.to === B.to),
+        },
+      };
+    } else if (enkrino.current === nid) {
+      return enkrino;
+    } else {
+      let eset = enkrino.graph.edges;
+      eset.push({
+        from: nid,
+        to: enkrino.current,
+        spec: {
+          temporary: true,
+        },
+      });
+      newenkrino = {
+        ...enkrino,
+        current: nid,
+        currentAuth: enkrino.graph.nodes.find(e=>e.id===nid).auth,
+        graph: {
+          ...enkrino.graph,
+          edges: _.uniqWith(
+            eset,
+            (A, B) =>
+              A.from === B.from &&
+              A.to === B.to &&
+              A.spec.temporary === B.spec.temporary
+          ),
+        },
+        mirror: {
+          ...enkrino.mirror,
+          nodes: enkrino.mirror.nodes.map((e) => e.id!==nid?e:{
+            ...e,
+            stack: e.stack.concat([context])
+          }),
+        },
+      };
+    }
+    return newenkrino;
+  },
+  nexts: (enkrino) => {
+    let current = enkrino.current ? enkrino.current : enkrino.start;
+    let forwards = successors(enkrino.graph, current);
+    let backwards = [];
+    switch (enkrino.graph.nodes.find((e) => e.id === current).backward) {
+      case 0:
+        break;
+      case 1:
+        backwards = successors(enkrino.mirror, current);
+        break;
+      default:
+        backwards = reachableNodes(enkrino.mirror, current);
+    }
+    return {
+      forwards: forwards,
+      backwards: backwards,
+    };
+  },
+  finish: (enkrino) => {
+    let newenkrino = {
+      ...enkrino,
+      status: "finished",
+    };
+    return newenkrino;
+  },
+};
+
+const a={
+	"graph": {
+		"edges": [
+			{
+				"to": "2",
+				"from": "1",
+        "spec":{
+          "temporary": false
+        }
+			},
+			{
+				"to": "3",
+				"from": "2",
+        "spec":{
+          "temporary": false
+        }
+			},
+			{
+				"to": "4",
+				"from": "2",
+        "spec":{
+          "temporary": false
+        }
+			}
+		],
+		"nodes": [
+			{
+				"id": "1",
+				"auth": "saler",
+				"name": "填写"
+			},
+			{
+				"id": "2",
+				"auth": "saler",
+				"name": "投标"
+			},
+			{
+				"id": "3",
+				"auth": "manager",
+				"name": "标记中标"
+			},
+			{
+				"id": "4",
+				"auth": "manager",
+				"name": "标记流标"
+			}
+		]
+	},
+	"start": "1"
+}
+
+m.start(a,'')
--- frontend/src/components/DateSheet.js
+++ frontend/src/components/DateSheet.js
@@ -36,7 +36,8 @@ const traverse = (path, title, schema, uiSchema, items, parent) => {
         path,
         title: schema.title ? schema.title : title,
         child: [],
-        parent
+        parent,
+        uiSchema
       }) - 1
       if (parent > -1) items[parent].child.push(index)
       const properties = schema.properties
@@ -65,102 +66,6 @@ const traverse = (path, title, schema, uiSchema, items, parent) => {
   }
 }
 
-// const isArrayDescendant = (curent, items) => {
-//   if (curent === -1) {
-//     return false
-//   } else {
-//     if (items[curent].type === arrayType) {
-//       return true
-//     } else if (items[curent].hasOwnProperty('arrayDescendant')) {
-//       return items[curent].arrayDescendant
-//     }
-//   }
-//   return isArrayDescendant(items[curent].parent, items)
-// }
-
-// const getRenderChild = (current, items, renderChild) => {
-//   const { child } = items[current]
-//   for (const c of child) {
-//     const { type } = items[c]
-//     const findType = basicTypes.find(e => e === type)
-//     if (findType !== undefined) {
-//       renderChild.push(c)
-//     } else if (type === objectType) {
-//       getRenderChild(c, items, renderChild)
-//     } else if (type === arrayType) {
-//       renderChild.push(c)
-//     }
-//   }
-// }
-
-// const map = (items, maps) => {
-//   for (const i in items) {
-//     items[i] = { ..._.cloneDeep(items[i]), isArrayDescendant: isArrayDescendant(items[i].parent, items) }
-//     if (items[i].type === arrayType) {
-//       const renderChild = []
-//       getRenderChild(i, items, renderChild)
-//       items[i] = { ..._.cloneDeep(items[i]), renderChild }
-//     }
-//   }
-//   items.forEach((value, index) => {
-//     if (value.hasOwnProperty('uiSchema')) {
-//       const uiSchema = value.uiSchema
-//       if (uiSchema) {
-//         if (uiSchema.hasOwnProperty('ui:location')) {
-//           const location = uiSchema['ui:location']
-//           if (location) {
-//             const { row, col } = location
-//             if (row && col) {
-//               maps.push({
-//                 location: { row, col },
-//                 index
-//               })
-//             }
-//           }
-//         }
-//       }
-//     }
-//     const { type, isArrayDescendant } = value
-//     if (!isArrayDescendant && type === arrayType) {
-//       maps.push({
-//         location: { row: 0, col: 0 },
-//         index
-//       })
-//     }
-//   })
-// }
-
-// const getMaxRowAndCol = (maps) => {
-//   var row = 0, col = 0
-//   for (const m of maps) {
-//     if (m.location.row > row) row = m.location.row
-//     if (m.location.col > col) col = m.location.col
-//   }
-//   return { row, col }
-// }
-
-// const createGrid = (maps, items) => {
-//   const { row: maxRow, col: maxCol } = getMaxRowAndCol(maps)
-//   const newGrid = []
-//   for (var i = 0; i < maxRow; i++) {
-//     newGrid[i] = []
-//     for (var j = 0; j < maxCol; j++) {
-//       newGrid[i][j] = { width: 100 }
-//     }
-//   }
-//   for (const i in maps) {
-//     const { location: { row, col }, index } = maps[i]
-//     if (row > 0 && col > 0) {
-//       newGrid[row - 1][col - 1] = { ...newGrid[row - 1][col - 1], ...maps[i], title: items[index].title, value: null, type: items[index].type }
-//     } else {
-//       const newRow = newGrid.push([{ width: 100, ...maps[i], title: items[index].title, value: null, type: items[index].type }])
-//       maps[i] = { ...maps[i], location: { row: newRow, col: 1 } }
-//       newGrid[newRow - 1][0] = { ...newGrid[newRow - 1][0], ...maps[i] }
-//     }
-//   }
-//   return newGrid
-// }
-
 const validateType = (value, type) => {
   if (type === 'number' || type === 'integer') {
     if (value === '' || value == null) return false
@@ -181,93 +86,6 @@ const validateType = (value, type) => {
   }
 }
 
-// function customizer(objValue, srcValue) {
-//   if (_.isObject(objValue) && _.isObject(srcValue) && !_.isArray(objValue) && !_.isArray(srcValue)) {
-//     for (const key in srcValue) {
-//       if (objValue.hasOwnProperty(key)) {
-//         const res = { ..._.cloneDeep(objValue), ..._.cloneDeep(srcValue), [key]: _.mergeWith(_.cloneDeep(objValue[key]), _.cloneDeep(srcValue[key]), customizer) }
-//         return res
-//       }
-//     }
-//   } else if (_.isArray(objValue) && _.isArray(srcValue)) {
-//     return objValue.concat(srcValue);
-//   }
-// }
-
-// const getData = (items, grid) => {
-//   var root = null
-//   var temp = []
-//   for (var i = 0; i < grid.length; i++) {
-//     for (var j = 0; j < grid[i].length; j++) {
-//       if (grid[i][j].hasOwnProperty('index')) {
-//         var { index, value, type } = grid[i][j]
-//         if (value || type === 'null' || type === 'boolean') {
-//           const { flag, value: newValue } = validateType(value, type)
-//           value = newValue
-//           if (flag) {
-//             var res = findPosiotn(index, items, value)
-//             temp.push(res)
-//             root = _.mergeWith(root, res, customizer)
-//           }
-//         }
-//       }
-//     }
-//   }
-//   return root
-// }
-
-// const findPosiotn = (index, items, value) => {
-//   var res = value
-//   var current = index
-//   while (items[current].parent !== -1) {
-//     const { parent, title } = items[current]
-//     const { type: parentType } = items[parent]
-//     if (parentType === objectType) {
-//       res = { [title]: res }
-//     } else if (parentType === arrayType) {
-//       res = [_.cloneDeep(res)]
-//     }
-//     current = parent
-//   }
-//   return res
-// }
-
-// const findPosition = (paths, currentIndex, schema, value) => {
-//   var res = null
-//   if (schema.hasOwnProperty('type')) {
-//     const { type } = schema
-//     const findType = basicTypes.find(e => e === type)
-//     if (findType !== undefined) {
-//       res = value
-//     } else if (type === objectType) {
-//       const key = paths[currentIndex + 1]
-//       res = { [key]: findPosition(paths, currentIndex + 1, schema.properties[key], value) }
-//     } else if (type === arrayType) {
-
-//     }
-//   }
-//   return res
-// }
-
-// const getData = (items, maps, grid, schema) => {
-//   var root = null
-//   for (const m of maps) {
-//     const { location: { row, col }, index } = m
-//     var { value } = grid[row - 1][col - 1]
-//     const { type, path } = items[index]
-//     if (value || type === 'null' || type === 'boolean') {
-//       const { flag, value: newValue } = validateType(value, type)
-//       value = newValue
-//       if (flag) {
-//         const paths = path.split('->')
-//         const res = findPosition(paths, 0, schema, value)
-//         root = _.merge(root, res)
-//       }
-//     }
-//   }
-//   return root
-// }
-
 function Check(props) {
   const { setStatus, checked, location } = props
   return (
@@ -279,28 +97,6 @@ function Check(props) {
     ></Checkbox>
   )
 }
-
-// const createNewItems = (currentItemIndex, items,) => {
-//   const res = []
-//   for (const index of items[currentItemIndex].renderChild) {
-//     const { title, type } = items[index]
-//     var item = { width: 100, title, value: null, type, index }
-//     if (type === 'boolean') {
-//       item = {
-//         ...item,
-//         value: false,
-//         component: null
-//       }
-//     } else if (type === arrayType) {
-//       item = {
-//         ...item,
-//         component: null
-//       }
-//     }
-//     res.push(item)
-//   }
-//   return res
-// }
 
 function AddButton(props) {
   const { path, setNewAddItems } = props
@@ -342,26 +138,6 @@ const updateComponent = (type, location, setStatus, value) => {
   return null
 }
 
-// const addComponent = (maps, items, newGrid, setStatus, setNewAddItems) => {
-//   for (const m of maps) {
-//     const { location: { row, col }, index } = m
-//     if (items[index].type === 'boolean') {
-//       newGrid[row - 1][col - 1] = {
-//         ...newGrid[row - 1][col - 1],
-//         value: false,
-//         component:
-//           <Check checked={false} setStatus={setStatus} row={row} col={col} />
-//       }
-//     } else if (items[index].type === arrayType) {
-//       newGrid[row - 1][col - 1] = {
-//         ...newGrid[row - 1][col - 1],
-//         childGrid: [],
-//         component:
-//           <AddButton items={items} currentItemIndex={index} row={row} setNewAddItems={setNewAddItems} location={{ row, col }} />
-//       }
-//     }
-//   }
-// }
 
 const getDataTree = (current, typeTree, dataTree) => {
   const dataTreeIndex = dataTree.push({ typeTreeIndex: current }) - 1
@@ -417,25 +193,18 @@ const initializeGrid = (typeTree, currentNode, grid, setStatus, setNewAddItems, 
   const findType = basicTypes.find(element => element === type)
   if (findType !== undefined) {
     const node = { ...cellOptions, value: null, type, title, path: [...path] }
-    if (uiSchema !== undefined && uiSchema !== null) {
-      if (uiSchema.hasOwnProperty('ui:location')) {
-        const { row, col } = uiSchema['ui:location']
-        grid[row - 1][col - 1] = {
-          ...grid[row - 1][col - 1],
-          ...node
+    if (!isArrayDescendant) {
+      if (uiSchema !== undefined && uiSchema !== null) {
+        if (uiSchema.hasOwnProperty('ui:location')) {
+          const { row, col } = uiSchema['ui:location']
+          grid[row - 1][col - 1] = {
+            ...grid[row - 1][col - 1],
+            ...node
+          }
         }
-        // if (type === 'boolean') {
-        //   grid[row - 1][col - 1] = {
-        //     ...grid[row - 1][col - 1],
-        //     ...node,
-        //     value: false,
-        //     component:
-        //       <Check checked={false} location={{ row, col }} setStatus={setStatus} />
-        //   }
-        // }
-      } else {
-        itemsRow.push(node)
       }
+    } else {
+      itemsRow.push({ ...node })
     }
   } else if (type === objectType) {
     const { child } = currentNode
@@ -469,12 +238,115 @@ const initializeGrid = (typeTree, currentNode, grid, setStatus, setNewAddItems, 
       })
     })
     if (newItemsRow.length > 0) {
-      grid.push(newItemsRow)
+      if (currentNode.hasOwnProperty('descendantDataTree')) {
+        const { typeTreeIndex } = currentNode.descendantDataTree[0]
+        console.log('typeTreeIndex', typeTreeIndex)
+        const { uiSchema: itemUiSchema } = typeTree[typeTreeIndex]
+        if (itemUiSchema) {
+          if (itemUiSchema.hasOwnProperty('ui:location') && itemUiSchema.hasOwnProperty('ui:span')) {
+            const { row, col } = itemUiSchema['ui:location']
+            const { rowSpan, colSpan } = itemUiSchema['ui:span']
+            console.log(row, col, rowSpan, colSpan)
+            var currentRow = row
+            var currentCol = col
+            for (var item of newItemsRow) {
+              if (currentCol - col + 1 > colSpan) {
+                currentRow++
+                currentCol = col
+              }
+              if (currentRow - row + 1 <= rowSpan) {
+                console.log(currentRow, currentCol, item)
+                if (grid.length < currentRow) {
+                  const l = grid.length
+                  for (var i = 1; i <= currentRow - l; i++) {
+                    grid.push([{ ...cellOptions }])
+                  }
+                }
+                if (grid[currentRow - 1].length < currentCol) {
+                  const l = grid[currentRow - 1].length
+                  for (var i = 1; i <= currentCol - l; i++) {
+                    grid[currentRow - 1].push({ ...cellOptions })
+                  }
+                }
+                grid[currentRow - 1][currentCol - 1] = item
+                currentCol++
+              } else {
+                break
+              }
+            }
+          } else {
+            grid.push(newItemsRow)
+          }
+        } else {
+          grid.push(newItemsRow)
+        }
+      }
     }
   }
 }
 
-const addComponents = (grid,setStatus) => {
+// const initializeGrid = (typeTree, currentNode, grid, setStatus, setNewAddItems, setDeleteItems, path = [0], itemsRow = [], isArrayDescendant = false) => {
+//   const { type, title, uiSchema } = typeTree[currentNode.typeTreeIndex]
+//   const findType = basicTypes.find(element => element === type)
+//   if (findType !== undefined) {
+//     const node = { ...cellOptions, value: null, type, title, path: [...path] }
+//     if (uiSchema !== undefined && uiSchema !== null) {
+//       if (uiSchema.hasOwnProperty('ui:location')) {
+//         const { row, col } = uiSchema['ui:location']
+//         grid[row - 1][col - 1] = {
+//           ...grid[row - 1][col - 1],
+//           ...node
+//         }
+//         // if(uiSchema.hasOwnProperty('ui:span')){
+//         //   const {rowSpan,colSpan}=uiSchema['ui:span']
+//         //   grid[row - 1][col - 1] = {
+//         //     ...grid[row - 1][col - 1],
+//         //     rowSpan,
+//         //     colSpan
+//         //   }
+//         // }
+//       } else {
+//         itemsRow.push(node)
+//       }
+//     }
+//   } else if (type === objectType) {
+//     const { child } = currentNode
+//     child.forEach((value, i) => {
+//       initializeGrid(typeTree, value, grid, setStatus, setNewAddItems, setDeleteItems, [...path, i], itemsRow, isArrayDescendant)
+//     })
+//   } else if (type === arrayType) {
+//     const node = {
+//       ...cellOptions, type, title, value: null,
+//       path: [...path],
+//       component:
+//         <AddButton path={[...path]} setNewAddItems={setNewAddItems} />
+//     }
+//     if (!isArrayDescendant) {
+//       grid.push([{ ...node }])
+//     } else {
+//       itemsRow.push({ ...node })
+//     }
+//     const { items } = currentNode
+//     const newItemsRow = []
+//     items.forEach((value, i) => {
+//       initializeGrid(typeTree, value[0], grid, setStatus, setNewAddItems, setDeleteItems, [...path, i], newItemsRow, true)
+//       newItemsRow.push({
+//         ...cellOptions,
+//         value: null,
+//         type: 'delete',
+//         title: 'delete',
+//         path: [...path, i],
+//         component:
+//           <DeleteButton path={[...path, i]} setDeleteItems={setDeleteItems} />
+//       })
+//     })
+//     if (newItemsRow.length > 0) {
+//       grid.push(newItemsRow)
+//     }
+//   }
+// }
+
+const addComponents = (grid, setStatus) => {
   for (var i = 0; i < grid.length; i++) {
     for (var j = 0; j < grid[i].length; j++) {
       const { type } = grid[i][j]
@@ -483,7 +355,7 @@ const addComponents = (grid,setStatus) => {
           ...grid[i][j],
           value: false,
           component:
-            <Check checked={false} location={{ row:i+1, col:j+1 }} setStatus={setStatus} />
+            <Check checked={false} location={{ row: i + 1, col: j + 1 }} setStatus={setStatus} />
         }
       }
     }
@@ -553,10 +425,10 @@ const findPosition = (root, value, typeTree, currentNode, path, current = 0, isA
     }
   } else if (type === arrayType) {
     if (isArrayDescendant) {
-      findPosition(root[path[current++]], value, typeTree, currentNode.items[path[current]][0], path, current,true)
+      findPosition(root[path[current++]], value, typeTree, currentNode.items[path[current]][0], path, current, true)
     } else {
       current++
-      findPosition(root[title],value,typeTree, currentNode.items[path[current]][0], path, current,true)
+      findPosition(root[title], value, typeTree, currentNode.items[path[current]][0], path, current, true)
     }
   }
 }
@@ -619,8 +491,6 @@ const getDataStructure = (currentNode, typeTree, root, isArrayDescendant = false
 
 export default function ({ schema, uiSchema }) {
   const classes = useStyles()
-  const [items, setItems] = React.useState([])
-  const [maps, setMaps] = React.useState([])
   const [grid, setGrid] = React.useState([])
   const [status, setStatus] = React.useState({ location: { row: -1, col: -1 }, value: null })
   const [newAddItems, setNewAddItems] = React.useState(null)
@@ -640,28 +510,14 @@ export default function ({ schema, uiSchema }) {
     }
   }, [schema, uiSchema])
 
-  // React.useEffect(() => {
-  //   const items = []
-  //   traverse('root', 'root', schema, uiSchema, items, -1)
-  //   setItems(items)
-  //   const maps = []
-  //   map(items, maps)
-  //   setMaps(maps)
-  //   var newGrid = createGrid(maps, items)
-  //   addComponent(maps, items, newGrid, setStatus, setNewAddItems)
-  //   setGrid(newGrid)
-  // }, [schema, uiSchema])
-
   React.useEffect(() => {
     var { location: { row, col }, value } = status
-    row = row - 1
-    col = col - 1
     if (row >= 0 && col >= 0) {
       var newGrid = grid.map(r => [...r]);
-      newGrid[row][col].value = value
-      newGrid[row][col] = {
-        ...newGrid[row][col],
-        component: updateComponent(newGrid[row][col].type, { row, col }, setStatus, value)
+      newGrid[row - 1][col - 1].value = value
+      newGrid[row - 1][col - 1] = {
+        ...newGrid[row - 1][col - 1],
+        component: updateComponent(newGrid[row - 1][col - 1].type, { row, col }, setStatus, value)
       }
       setGrid(newGrid)
     }
@@ -682,49 +538,11 @@ export default function ({ schema, uiSchema }) {
       getMaxRowCol(dataTree[0], typeTree, max)
       var newGrid = init(max)
       initializeGrid(typeTree, dataTree[0], newGrid, setStatus, setNewAddItems, setDeleteItems)
-      addComponents(newGrid,setStatus)
+      addComponents(newGrid, setStatus)
       setGrid(newGrid)
       console.log(typeTree, dataTree, newGrid)
     }
   }, [dataTree])
-
-  // React.useEffect(() => {
-  //   if (newAddItems) {
-  //     const { row, items: newCells, location } = newAddItems
-  //     var newGrid = _.cloneDeep(grid)
-  //     const deleteArray = []
-  //     for (const cell of newCells) {
-  //       const col = newGrid[row - 1].push(cell)
-  //       deleteArray.push({ row, col })
-  //       const { type, index } = cell
-  //       if (type === 'boolean') {
-  //         newGrid[row - 1][col - 1] = {
-  //           ...newGrid[row - 1][col - 1],
-  //           component:
-  //             <Check checked={false} setStatus={setStatus} row={row} col={col} />
-  //         }
-  //       } else if (type === arrayType) {
-  //         newGrid[row - 1][col - 1] = {
-  //           ...newGrid[row - 1][col - 1],
-  //           childGrid: [],
-  //           component:
-  //             <AddButton items={items} currentItemIndex={index} row={row} setNewAddItems={setNewAddItems} location={{ row, col }} />
-  //         }
-  //       }
-  //     }
-  //     const col = newGrid[row - 1].push({
-  //       width: 100, title: 'deleteBtn', value: null, type: 'delete'
-  //     })
-  //     deleteArray.push({ row, col })
-  //     newGrid[row - 1][col - 1] = {
-  //       ...newGrid[row - 1][col - 1],
-  //       component:
-  //         <DeleteButton deleteArray={deleteArray} setDeleteItems={setDeleteItems} />
-  //     }
-  //     const deleteIndex = newGrid[location.row - 1][location.col - 1].childGrid.push(deleteArray) - 1
-  //     setGrid(newGrid)
-  //   }
-  // }, [newAddItems])
 
   React.useEffect(() => {
     if (deleteItems) {
@@ -733,10 +551,6 @@ export default function ({ schema, uiSchema }) {
       setDataTree(newDataTree)
     }
   }, [deleteItems])
-
-  // console.log('items', items)
-  // console.log('maps', maps)
-  // console.log('grid', grid)
 
   return (
     <>
@@ -773,9 +587,6 @@ export default function ({ schema, uiSchema }) {
         variant="contained"
         onClick={() => {
           console.log(getData(grid, dataTree, typeTree))
-          // var root = {}
-          // getDataStructure(dataTree[0], typeTree, root)
-          // console.log(root)
         }}
       >
         submit
